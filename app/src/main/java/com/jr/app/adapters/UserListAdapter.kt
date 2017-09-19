6f87c5f2e0d89f4e5d5b0dd7027fab5ff90d01b1
Jonathan <jono1408@gmail.com>
2017-09-19 16:54:04 +0100

    building the fragment activity logic that gets the data from the server and populates the recycleView

--- app/src/main/java/com/jr/app/adapters/UserListAdapter.kt
+++ app/src/main/java/com/jr/app/adapters/UserListAdapter.kt
@@ -1,30 +1,48 @@
 package com.jr.app.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.jr.app.R
+import com.jr.app.models.ExampleData
+import kotlinx.android.synthetic.main.user_list_item.view.*
 
 /**
  * Created by Jonathan on 12/09/2017.
  */
 
-class UserListAdapter: RecyclerView.Adapter<UserListAdapter.ViewHolder>{
-    constructor() : super()
+class UserListAdapter : RecyclerView.Adapter<UserListAdapter.ViewHolder> {
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    var userListItems: List<ExampleData>? = null
+
+    constructor(userListItemData: List<ExampleData>) {
+        userListItems = userListItemData
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.user_list_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userData = userListItems!![position]
+        holder.firstName!!.text = userData.firstName
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+      return  userListItems!!.size
     }
 
-    inner class ViewHolder: RecyclerView.ViewHolder{
-        constructor(itemView: View?) : super(itemView)
+    inner class ViewHolder : RecyclerView.ViewHolder {
+        var firstName: TextView? = null
+        var surnName: TextView? = null
+
+        constructor(itemView: View) : super(itemView) {
+            firstName = itemView.first_name_text
+            surnName = itemView.second_name_text
+        }
 
     }
 }
--- app/src/main/java/com/jr/app/ui/ShowAllUserList.kt
+++ app/src/main/java/com/jr/app/ui/ShowAllUserList.kt
@@ -1,16 +1,39 @@
 package com.jr.app.ui
 
 import android.app.Fragment
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jr.app.R
+import com.jr.app.adapters.UserListAdapter
+import com.jr.app.models.ExampleData
+import com.jr.app.network.RestService
+import kotlinx.android.synthetic.main.list_view_screen.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * Created by Jonathan on 11/09/2017.
  */
 class ShowAllUserList : Fragment() {
+
+    var listUsers : List<ExampleData>? = null
+
+    inner class CallbackAllUsers : Callback<List<ExampleData>> {
+        override fun onFailure(call: Call<List<ExampleData>>?, t: Throwable?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onResponse(call: Call<List<ExampleData>>?, response: Response<List<ExampleData>>?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +43,17 @@ class ShowAllUserList : Fragment() {
                               savedInstanceState: Bundle?): View? {
         var view: View = inflater.inflate(R.layout.add_user_layout, null)
         return view;
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        recyclerView.layoutManager = LinearLayoutManager(activity.applicationContext)
+        recyclerView.adapter = UserListAdapter(listUsers!!)
+
+    }
+
+    private fun getListOfUsers(){
+        RestService().getAllUsers(CallbackAllUsers())
     }
 
 }
